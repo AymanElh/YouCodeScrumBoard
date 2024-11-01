b3c5dccaaa17cce749c442d695024cd5a69d6f38
unknown <elhamioui1964@gmail.com>
2024-11-01 21:39:29 +0100

    Optimize display and close modal code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
-
-
 // Display and Close form modal
 
 const addTaskBtn = document.getElementById("addTaskBtn");
 const modalTask = document.getElementById("modal-task");
 const app = document.getElementById("app");
 const closeBtn = document.querySelector(".btn-close");
+const cancelBtn = document.getElementById("cancel-modal")
 
-addTaskBtn.addEventListener("click", () => {
+function showModal() {
   modalTask.style.display = "flex";
-})
-
-closeBtn.addEventListener("click", () => {
+}
+function hideModal() {
   modalTask.style.display = "none";
+}
+
+addTaskBtn.addEventListener("click", showModal)
+closeBtn.addEventListener("click", hideModal)
+cancelBtn.addEventListener("click", hideModal)
+
+window.addEventListener("click", (event) => {
+  if(event.target === modalTask) {hideModal()};
 })
